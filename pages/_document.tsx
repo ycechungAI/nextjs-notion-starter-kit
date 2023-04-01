2198cfb4b9d3c37963f034db6501e3183fd78a63
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2023-04-01 17:16:01 -0400

    feat: config template
    - update site.config.ts
    - update web manifest & favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,15 +9,31 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel='shortcut icon' href='/favicon.ico' />
+            <link
+              rel='apple-touch-icon'
+              sizes='180x180'
+              href='/apple-touch-icon.png'
+            />
             <link
               rel='icon'
               type='image/png'
               sizes='32x32'
-              href='favicon.png'
+              href='/favicon-32x32.png'
             />
-
-            <link rel='manifest' href='/manifest.json' />
+            <link
+              rel='icon'
+              type='image/png'
+              sizes='16x16'
+              href='/favicon-16x16.png'
+            />
+            <link rel='manifest' href='/site.webmanifest' />
+            <link
+              rel='mask-icon'
+              href='/safari-pinned-tab.svg'
+              color='#5bbad5'
+            />
+            <meta name='msapplication-TileColor' content='#2b5797' />
+            <meta name='theme-color' content='#ffffff' />
           </Head>
 
           <body>
